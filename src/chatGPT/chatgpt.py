923e5e01be2c68c21c116e076496d0d130539571
Cristian Chivisky <cristian.chivisky@gmail.com>
2023-04-11 14:28:50 -0300

    Se agregaron estructuras Try/Except

--- src/chatGPT/chatgpt.py
+++ src/chatGPT/chatgpt.py
@@ -13,17 +13,29 @@ TEMPERATURE=0.75
 NMAX=1
 MODEL_ENGINE = "text-davinci-003"
 
-userText=input('Introduce una pregunta ')
-if userText != '':
-    userText='You: '+ userText
-    print(f'{userText} \n')
-    completion=openai.Completion.create(engine=MODEL_ENGINE,
-                                        prompt=userText,
-                                        max_tokens=MAX_TOKENS,
-                                        n=NMAX,
-                                        top_p=TOP_P,
-                                        frequency_penalty=FREQ_PENALTY,
-                                        presence_penalty=PRES_PENALTY,
-                                        temperature=TEMPERATURE,
-                                        stop=STOP)
-    print(f'chatGPT: {completion.choices[0].text}')
+try:
+    userText=input('Introduce una pregunta ')
+    if userText != '':
+        raise ValueError('Debe ingresar una consulta')
+except ValueError as e:
+    print(e)
+else:
+    try:
+        userText='You: '+ userText
+        print(f'{userText} \n')
+    except:
+        print('Ha habido un error')
+    try:
+        completion=openai.Completion.create(engine=MODEL_ENGINE,
+                                            prompt=userText,
+                                            max_tokens=MAX_TOKENS,
+                                            n=NMAX,
+                                            top_p=TOP_P,
+                                            frequency_penalty=FREQ_PENALTY,
+                                            presence_penalty=PRES_PENALTY,
+                                            temperature=TEMPERATURE,
+                                            stop=STOP)
+    except:
+        print('Error al llamar a la API')
+    else:
+        print(f'chatGPT: {completion.choices[0].text}')
