773c36f2d0b9e1e0492d3010ff32dc35f7240349
Cristian Chivisky <cristian.chivisky@gmail.com>
2023-04-13 10:49:27 -0300

    Se agregan ultimas modificaciones al archivo chatgpt.py

--- src/chatGPT/chatgpt.py
+++ src/chatGPT/chatgpt.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import sys
 sys.path.append(r'c:\users\crist\appdata\local\programs\python\python37\lib\site-packages')
 import openai
@@ -7,35 +8,57 @@ openai.api_key = clave
 TOP_P=1
 FREQ_PENALTY=0
 PRES_PENALTY=0
-STOP=None
+STOP=["You: ", "chatGPT: "]
 MAX_TOKENS=1024
 TEMPERATURE=0.75
 NMAX=1
 MODEL_ENGINE = "text-davinci-003"
+USERNAME='You: '
+AI_NAME='chatGPT: '
+def get_completion(userText):
+    completion=openai.Completion.create(engine=MODEL_ENGINE,
+                                        prompt=userText,
+                                        max_tokens=MAX_TOKENS,
+                                        n=NMAX,
+                                        top_p=TOP_P,
+                                        frequency_penalty=FREQ_PENALTY,
+                                        presence_penalty=PRES_PENALTY,
+                                        temperature=TEMPERATURE,
+                                        stop=STOP)
+    return completion.choices[0].text
+def start_conversation():
+    print('Bienvenido al modo conversación, para salir ingrese "exit"')
+    conversation_history = ''
+    while True:
+        try:
+            user_text=input('You: ')
+            if user_text == '':
+                raise ValueError('Debe ingresar una consulta!')
+            elif user_text=='exit':
+                break
+        except ValueError as e:
+            print(e)
+        else:
+            conversation_history += f'\n {USERNAME}{user_text}\n {AI_NAME}'
+            try:
+                message=get_completion(conversation_history)
+            except:
+                print('Error en la llamada a la API')
+            else:
+                conversation_history += message
+                print(f'{AI_NAME}{message.strip()}\n')
 
-try:
-    userText=input('Introduce una pregunta ')
-    if userText != '':
-        raise ValueError('Debe ingresar una consulta')
-except ValueError as e:
-    print(e)
+if len(sys.argv) > 1:
+   aux=str(sys.argv[1])
+   if aux=='--convers':
+    try:
+        start_conversation()
+    except:
+        print('Error en start_conversation')
 else:
+    print('Ingrese su consulta')
+    aux=input('You: ')
     try:
-        userText='You: '+ userText
-        print(f'{userText} \n')
+        print(f'chatGPT: {get_completion(aux)}')
     except:
-        print('Ha habido un error')
-    try:
-        completion=openai.Completion.create(engine=MODEL_ENGINE,
-                                            prompt=userText,
-                                            max_tokens=MAX_TOKENS,
-                                            n=NMAX,
-                                            top_p=TOP_P,
-                                            frequency_penalty=FREQ_PENALTY,
-                                            presence_penalty=PRES_PENALTY,
-                                            temperature=TEMPERATURE,
-                                            stop=STOP)
-    except:
-        print('Error al llamar a la API')
-    else:
-        print(f'chatGPT: {completion.choices[0].text}')
+        print('Error en la llamada a la API')
