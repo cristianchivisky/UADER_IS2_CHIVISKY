2b630f5967e473e5bf00f902b59f96dd96276c26
Cristian Chivisky <cristian.chivisky@gmail.com>
2023-06-22 20:40:49 -0300

    Modificacion de getJason.py segun punto 3 del TP4 RRR

--- src/RRR_TP1/getJason.py
+++ src/RRR_TP1/getJason.py
@@ -1,18 +1,47 @@
 """Copyright IS2 © 2022, 2023 todos los derechos reservados."""
-#*----------------------------------------------------------------------------------
+#*-------------------------------------------------------------
 #getJason
-#Este programa permite extraer la clave de acceso API para utilizar los servicios del Banco xxx.
-#Este programa esta siendo obtenido por ingenieria reversa el 15/06/2023.
-#*-----------------------------------------------------------------------------------
-# uncompyle6 version 3.9.0
-# Python bytecode version base 2.7 (62211)
-# Decompiled from: Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18)
-# [MSC v.1900 64 bit (AMD64)]
-# Embedded file name: getJason.py
-# Compiled at: 2022-06-14 16:15:55
-#*-----------------------------------------------------------------------------------
+#*-------------------------------------------------------------
+from __future__ import annotations
+from collections.abc import Iterable, Iterator
+from typing import Any, List
 import json
 import sys
+
+class AlphabeticalOrderIterator(Iterator):
+    _position: int = None
+    _reverse: bool = False
+
+    def __init__(self, collection: WordsCollection, reverse: bool = False) -> None:
+        """Inicializa el iterador."""
+        self._collection = collection
+        self._reverse = reverse
+        self._position = -1 if reverse else 0
+
+    def __next__(self):
+        """Este metodo obtiene siguiente elemento."""
+        try:
+            value = self._collection[self._position]
+            self._position += -1 if self._reverse else 1
+        except IndexError:
+            raise StopIteration()
+
+        return value
+
+class WordsCollection(Iterable):
+    """Inicializa la colección de palabras."""
+    def __init__(self, collection: List[Any] = []) -> None:
+        self._collection = collection
+
+    def __iter__(self) -> AlphabeticalOrderIterator:
+        return AlphabeticalOrderIterator(self._collection)
+
+    def get_reverse_iterator(self) -> AlphabeticalOrderIterator:
+        return AlphabeticalOrderIterator(self._collection, True)
+
+    def add_item(self, item: Any):
+        """Agrega un elemento a la coleccion"""
+        self._collection.append(item)
 
 class SingletonMeta(type):
     """Clase que implementa el patron Singleton."""
@@ -32,10 +61,10 @@ class APItoken(metaclass=SingletonMeta):
     def __init__(self):
         """Este metodo se ejecuta al crear una instancia de la clase"""
 
-    def extraer_token(self, jsonfile, jsonkey):
+    def extraer_token(self, jsonkey):
         """Este metodo permite extraer un token de un archivo JSON."""
         try:
-            with open(jsonfile, 'r') as myfile:
+            with open("sitedata.json", 'r') as myfile:
                 data = myfile.read()
         except FileNotFoundError:
             print('No existe el archivo JSON')
@@ -45,7 +74,7 @@ class APItoken(metaclass=SingletonMeta):
             sys.exit()
         try:
             obj = json.loads(data)
-            print(str(obj[jsonkey]))
+            return str(obj[jsonkey])
         except KeyError:
             print('Token incorrecto, no existe en el archivo JSON')
             sys.exit()
@@ -53,18 +82,62 @@ class APItoken(metaclass=SingletonMeta):
             print('Error al extraer el token del archivo JSON')
             sys.exit()
 
-    def obtener_mensaje_ayuda(self):
-        """Este metodo imprime un mensaje de ayuda en la consola."""
-        print('Mensaje de ayuda:')
-        print('  Extractor de token para acceso API Servicios Banco XXX (version 1.0)\n')
-        print('  Este programa permite extraer la clave de acceso API para utilizar los servicios del Banco XXX.\n')
-        print('  El programa operara como un microservicio invocado mediante:')
-        print('  {path ejecutable}/getJason.pyc {path archivo JSON}/{nombre archivo JSON}.json')
-        print('  Ejemplo:')
-        print('  ./getJason.pyc ./sitedata.json\n')
-        print('  El token podra recuperarse mediante el standard output de ejecucion en el formato')
-        print('  XXXX-XXXX-XXXX-XXXX')
-        sys.exit()
+class Handler(object):
+
+    def __init__(self):
+        """Inicializa el objeto Handler."""
+        self.nextHandler = None
+
+    def handle(self, numero_pedido, monto):
+        """Maneja el pedido de pago."""
+        if self.nextHandler == None:
+            print("No hay saldo suficiente en las cuentas, no se puedo realizar el pago")
+            return
+        self.nextHandler.handle(numero_pedido, monto)
+
+class Cuenta1Handler(Handler):
+    def __init__(self):
+        """Inicializa el objeto Cuenta1Handler."""
+        super(Cuenta1Handler, self).__init__()
+        self.token=z1.extraer_token('token1')
+        self.monto=1000
+
+    def handle(self, numero_pedido, monto):
+        """Maneja el pedido de pago en el Banco 1."""
+        print(f"Banco 1: explora pedido de pago {numero_pedido}")
+        if monto <= self.monto:
+            self.procesar_pago(numero_pedido, monto)
+        else:
+            print("Saldo insuficiente, Banco 1: pasa al siguiente actuador")
+            super(Cuenta1Handler, self).handle(numero_pedido, monto)
+
+    def procesar_pago(self, numero_pedido, monto):
+        """Procesa el pago exitoso en el Banco 1."""
+        self.monto -= monto
+        print(f'Pago exitoso, numero de pedido {numero_pedido}, token: {self.token}, monto ${monto}\n')
+        collection.add_item(f'Numero de pedido {numero_pedido}, Token: {self.token}, Monto ${monto}')
+
+class Cuenta2Handler(Handler):
+    def __init__(self):
+        """Inicializa el objeto Cuenta2Handler."""
+        super(Cuenta2Handler, self).__init__()
+        self.token=z1.extraer_token('token2')
+        self.monto=2000
+
+    def handle(self, numero_pedido, monto):
+        """Maneja el pedido de pago en el Banco 2."""
+        print(f"Banco 2: explora pedido de pago {numero_pedido}")
+        if monto <= self.monto:
+            self.procesar_pago(numero_pedido, monto)
+        else:
+            print("Saldo insuficiente, Banco 2: pasa al siguiente actuador")
+            super(Cuenta2Handler, self).handle(numero_pedido, monto)
+
+    def procesar_pago(self, numero_pedido, monto):
+        """Procesa el pago exitoso en el Banco 2."""
+        self.monto -= monto
+        print(f'Pago exitoso, numero de pedido {numero_pedido}, token: {self.token}, monto: ${monto}\n')
+        collection.add_item(f'Numero de pedido {numero_pedido}, Token: {self.token}, Monto ${monto}')
 
 #*--------------------------------------------------------------
 # main
@@ -72,17 +145,19 @@ class APItoken(metaclass=SingletonMeta):
 if __name__ == "__main__":
 
     z1=APItoken() # Se crea una instancia de la clase APItoken
-    try:
-        if sys.argv[1] == '-h':
-            z1.obtener_mensaje_ayuda()
-        else:
-            JSONFILE = sys.argv[1] # Se obtienen los nombres de archivo JSON
-            JSONKEY = sys.argv[2] #y clave desde los argumentos de linea de comandos
-            z1.extraer_token(JSONFILE, JSONKEY)
-    except IndexError:
-        print('Argumento invalido')
-        sys.exit()
-        # Si se produce un error de indice en los argumentos de linea de comandos,
-        #  muestra un mensaje de error y se sale del programa
+    cuenta1=Cuenta1Handler() # Se inicializan los actuadores
+    cuenta2=Cuenta2Handler()
+    cuenta1.nextHandler=cuenta2 # Establece ahora la cadena de llamada
+    collection = WordsCollection()
 
-# okay decompiling getJason.pyc
+    cuenta1.handle('1', 500)
+    cuenta1.handle('2', 500)
+    cuenta1.handle('3', 500)
+    cuenta1.handle('4', 500)
+    cuenta1.handle('5', 500)
+    cuenta1.handle('6', 500)
+    cuenta1.handle('7', 500)
+
+    print("\nPagos realizados:")
+    print("\n".join(collection))
+    print("")
