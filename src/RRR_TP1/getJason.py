2972783a9d63cf8a59495d4cb328125266816a2b
Cristian Chivisky <cristian.chivisky@gmail.com>
2023-06-21 23:40:27 -0300

    Se utilizo la estrategia Branching by abtraction para hacer la convergencia entre el programa original y su version refactorizada

--- src/RRR_TP1/getJason.py
+++ src/RRR_TP1/getJason.py
@@ -12,11 +12,20 @@
 import json, sys
 
 if __debug__ :
-    print('__debug__ is True --> codigo nuevo')
+    print('__debug__ is True --> codigo nuevo\n')
 else :
-    print('__debug__ is False --> codigo viejo')
+    print('__debug__ is False --> codigo viejo\n')
 
-class APItoken:
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+class APItoken(metaclass=SingletonMeta):
     def __init__(self):
         pass
 
@@ -32,7 +41,7 @@ class APItoken:
             sys.exit()
         try:
             obj = json.loads(data)
-            return str(obj[jsonkey])
+            print(str(obj[jsonkey]))
         except KeyError:
             print('Token incorrecto, no existe en el archivo JSON')
             sys.exit()
@@ -52,39 +61,19 @@ class APItoken:
         print('  XXXX-XXXX-XXXX-XXXX')
         sys.exit()
 
-if __debug__:
-    extractor_de_token=APItoken()
-    try:
-        if sys.argv[1] == '-h':
-            extractor_de_token.obtener_mensaje_ayuda()
-        else:
-            jsonfile = sys.argv[1]
-            jsonkey = sys.argv[2]
-            token = extractor_de_token.extraer_token(jsonfile, jsonkey)
-            print(token)
-    except IndexError:
-        print('Argumento invalido')
-        sys.exit()
-else:
-    try:
-        if sys.argv[1] == '-h':
-            print('Mensaje de ayuda:')
-            print('  Extractor de token para acceso API Servicios Banco XXX (version 1.0)\n')
-            print('  Este programa permite extraer la clave de acceso API para utilizar los servicios del Banco XXX.\n')
-            print('  El programa operara como un microservicio invocado mediante:')
-            print('  {path ejecutable}/getJason.pyc {path archivo JSON}/{nombre archivo JSON}.json')
-            print('  Ejemplo:')
-            print('  ./getJason.pyc ./sitedata.json\n')
-            print('  El token podra recuperarse mediante el standard output de ejecucion en el formato')
-            print('  XXXX-XXXX-XXXX-XXXX')
-            sys.exit()
-        else:
-            jsonfile = sys.argv[1]
-            jsonkey = sys.argv[2]
-    except IndexError:
-        print('Argumento invalido')
-        sys.exit()
+def obtener_mensaje_ayuda_viejo():
+    print('Mensaje de ayuda:')
+    print('  Extractor de token para acceso API Servicios Banco XXX (version 1.0)\n')
+    print('  Este programa permite extraer la clave de acceso API para utilizar los servicios del Banco XXX.\n')
+    print('  El programa operara como un microservicio invocado mediante:')
+    print('  {path ejecutable}/getJason.pyc {path archivo JSON}/{nombre archivo JSON}.json')
+    print('  Ejemplo:')
+    print('  ./getJason.pyc ./sitedata.json\n')
+    print('  El token podra recuperarse mediante el standard output de ejecucion en el formato')
+    print('  XXXX-XXXX-XXXX-XXXX')
+    sys.exit()
 
+def extraer_token_viejo(jsonfile, jsonkey):
     try:
         with open(jsonfile, 'r') as (myfile):
             data = myfile.read()
@@ -104,5 +93,37 @@ else:
     except:
         print('Error al extraer el token del archivo JSON')
         sys.exit()
+
+class abstarctExtractorToken:
+    def __init__(self):
+        self.p1=APItoken()
+    
+    def extraer_token(self, jsonfile, jsonkey):
+        if __debug__:
+            self.p1.extraer_token(jsonfile, jsonkey)
+        else:
+            extraer_token_viejo(jsonfile, jsonkey)
+
+    def obtener_mensaje_ayuda(self):
+        if __debug__:
+            self.p1.obtener_mensaje_ayuda()
+        else:
+            obtener_mensaje_ayuda_viejo()
+
+
+#*--------------------------------------------------------------
+# main
+#*--------------------------------------------------------------
+z1=abstarctExtractorToken()
+try:
+    if sys.argv[1] == '-h':
+        z1.obtener_mensaje_ayuda()
+    else:
+        jsonfile = sys.argv[1]
+        jsonkey = sys.argv[2]
+        z1.extraer_token(jsonfile, jsonkey)
+except IndexError:
+    print('Argumento invalido')
+    sys.exit()
 
 # okay decompiling getJason.pyc
